5cdbb1d483753d066ce4d0bed7ca8cee84b2d454
Julien <julienbouin31@gmail.com>
2024-03-14 17:20:39 +0100

    add style css

--- WEB-INF/src/controleurs/Authent.java
+++ WEB-INF/src/controleurs/Authent.java
@@ -28,6 +28,50 @@ public class Authent extends HttpServlet {
                     "        <input type=\"submit\" value=\"Valider\">;\n" + //
                     "    </form>\n" + //
                     "</body>\n");
+            out.println("  <style>\n" + //
+                    "        body {\n" + //
+                    "            font-family: Arial, sans-serif;\n" + //
+                    "            background-color: #f4f4f4;\n" + //
+                    "            margin: 0;\n" + //
+                    "            padding: 0;\n" + //
+                    "            display: flex;\n" + //
+                    "            align-items: center;\n" + //
+                    "            justify-content: center;\n" + //
+                    "            height: 100vh;\n" + //
+                    "        }\n" + //
+                    "\n" + //
+                    "        form {\n" + //
+                    "            background-color: #fff;\n" + //
+                    "            padding: 20px;\n" + //
+                    "            border-radius: 8px;\n" + //
+                    "            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);\n" + //
+                    "        }\n" + //
+                    "\n" + //
+                    "        label {\n" + //
+                    "            display: block;\n" + //
+                    "            margin-bottom: 8px;\n" + //
+                    "        }\n" + //
+                    "\n" + //
+                    "        input {\n" + //
+                    "            width: 100%;\n" + //
+                    "            padding: 8px;\n" + //
+                    "            margin-bottom: 16px;\n" + //
+                    "            box-sizing: border-box;\n" + //
+                    "        }\n" + //
+                    "\n" + //
+                    "        button {\n" + //
+                    "            background-color: #4caf50;\n" + //
+                    "            color: #fff;\n" + //
+                    "            padding: 10px;\n" + //
+                    "            border: none;\n" + //
+                    "            border-radius: 4px;\n" + //
+                    "            cursor: pointer;\n" + //
+                    "        }\n" + //
+                    "\n" + //
+                    "        button:hover {\n" + //
+                    "            background-color: #45a049;\n" + //
+                    "        }\n" + //
+                    "    </style>");
         } catch (Exception e) {
             out.print(e.getMessage());
 
